179b21e790d56e4ab68dc5ef2cf0f9a16b78a2d4
Lucia Sandoval <asandovalp9@miumg.edu.gt>
2025-08-01 22:14:59 -0600

    Version Dos
    Historia basica con texto fijo

--- Historia.py
+++ Historia.py
@@ -1,1 +1,8 @@
 print("Generador de historias aleatorias")
+
+nombre = "Pedro"
+lugar = "el bosque"
+accion = "exploró"
+
+historia = f"{nombre} fue a {lugar} y {accion} algo misterioso."
+print(historia)
